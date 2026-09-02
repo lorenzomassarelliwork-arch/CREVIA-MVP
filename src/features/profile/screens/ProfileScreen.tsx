bc5927b096a722550c01edca83a3de3125afc7ef
Lorenzo Massarelli <lorenzo.massarelli.work@gmail.com>
2026-09-02 18:37:44 +0200

    feat: add MVP profile route

---
+++ src/features/profile/screens/ProfileScreen.tsx
@@ -0,0 +1,5 @@
+import PhaseScreen from '../../../components/PhaseScreen';
+
+export default function ProfileScreen() {
+  return <PhaseScreen title="Profilo" subtitle="Il profilo manterrà l'identità Crevia e mostrerà progressivamente progetti ed esperienze verificate." />;
+}
