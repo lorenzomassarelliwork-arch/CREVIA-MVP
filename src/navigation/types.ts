32cadf581235c337220773774642e24b34380973
Lorenzo Massarelli <lorenzo.massarelli.work@gmail.com>
2026-09-02 18:36:40 +0200

    feat: add MVP navigation types

---
+++ src/navigation/types.ts
@@ -0,0 +1,16 @@
+import type { NavigatorScreenParams } from '@react-navigation/native';
+
+export type MainTabParamList = {
+  Home: undefined;
+  Chat: undefined;
+  Search: undefined;
+  Profile: undefined;
+};
+
+export type RootStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  ForgotPassword: undefined;
+  Main: NavigatorScreenParams<MainTabParamList> | undefined;
+  ProjectDetail: { projectId: string };
+};
