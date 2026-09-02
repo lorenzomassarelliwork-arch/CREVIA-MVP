4cc71def27cb4e7c08cd3ef3e4dfd19c69c4a27c
Lorenzo Massarelli <lorenzo.massarelli.work@gmail.com>
2026-09-02 18:37:39 +0200

    feat: add MVP search route

---
+++ src/features/projects/screens/SearchScreen.tsx
@@ -0,0 +1,5 @@
+import PhaseScreen from '../../../components/PhaseScreen';
+
+export default function SearchScreen() {
+  return <PhaseScreen title="Cerca" subtitle="Ricerca progetti, ruoli e competenze: questo sarà uno dei flussi centrali dell'MVP." />;
+}
