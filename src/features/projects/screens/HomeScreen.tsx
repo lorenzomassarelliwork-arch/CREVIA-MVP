f93ec9c5275d679d286151028244a34264a5bf82
Lorenzo Massarelli <lorenzo.massarelli.work@gmail.com>
2026-09-02 18:37:24 +0200

    feat: add MVP home route

---
+++ src/features/projects/screens/HomeScreen.tsx
@@ -0,0 +1,5 @@
+import PhaseScreen from '../../../components/PhaseScreen';
+
+export default function HomeScreen() {
+  return <PhaseScreen title="Home" subtitle="Migrazione della Home Crevia in corso sulla nuova architettura MVP." />;
+}
